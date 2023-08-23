c2cd799b98a3fb5fef04ca2694405f738cd46121
prateeksanodiya0209 <prateeksanodiya717@gmail.com>
2023-08-23 11:57:27 +0530

    days-23-apiQues

---
+++ script.js
@@ -0,0 +1,11 @@
+// const fetchData = async(URL)=>{
+//     const response = await fetch(URL)
+//     const data = await response.json();
+//     console.log(data);
+// };
+// fetchData('https://jsonplaceholder.typicode.com/posts')
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((response) =>response.json())
+.then((data)=> console.log(data))
+.catch((error)=> console.log(error));
