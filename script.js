d03e1a5bd6e8a55c36ac6c7ea4257e174b9b66d8
prateeksanodiya0209 <prateeksanodiya717@gmail.com>
2023-08-26 11:21:57 +0530

    days-25/project1

--- script.js
+++ script.js
@@ -1,68 +1,97 @@
-const idInput = document.getElementById("idInput")
-const todoList = document.getElementById("todoList")
-const submitBtn = document.getElementById("submitBtn")
-const searchInput = document.getElementById("searchInput")
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const tableBody = document.getElementById("body");
+const head = document.getElementById("head");
 
-const todoLiData =[]
+const addNewColumn = (colunmData) =>{
+    tableData.header.push(colunmData);
+    tableData.body.forEach((tableRow ,idx) => {
+        const {colunmElements = []} =tableRow;
+        colunmElements.push({
+            elementType : 'td',
+            columnName : colunmData.columnName;
+            rowOrder : idx ,
+            value : `Cell ${colunmData.columnName}${idx}`
+        })
+    })
+};
+const dataStore = {
+  header: [
+    {
+      elementType: "th",
+      columnName: "A",
+      order: 1,
+      sort: "undefined",
+    },
+    { elementType: "th", columnName: "B", order: 1, sort: "undefined" },
+    { elementType: "th", columnName: "C", order: 1, sort: "undefined" },
+  ],
 
-const clearTodoList = ()=>{
-    todoList.innerHTML ='';
-}
+  body: [
+    {
+      elementType: "tr",
+      rowOrder: 1,
+      colunmElements: [
+        { elementType: "td",columnName: "Column A",order: 1,rowOrder: 1,value: "row1"},
+        { elementType: "td",columnName: "Column B",order: 2,rowOrder: 2, value: "row2"},
+        { elementType: "td",columnName: "Column C",order: 3,rowOrder: 3, value: "row3"}
+      ],
+}]
+};
 
-const updateTodoList = ()=>{
-    clearTodoList()
-    for(let i=0 ; i<todoLiData.length ; i++){
-        const liTagElement = document.createElement("li")
-        liTagElement.innerText = todoLiData[i];
-        todoList.append(liTagElement);
+const dataStoreAdd = () => {
+  const {header, body} = dataStore;
+  
+  for (let i = 0; i < header.length; i++) {
+    const createTh = document.createElement(header[i].elementType);
+    createTh.innerText = header[i].columnName;
+    head.append(createTh);
+  }
+  body.forEach((tableRow) => {
+    const {elementType,colunmElements} = tableRow;
+    const trElement = document.createElement(elementType);
+    colunmElements.forEach((cellElement) => {
+        const tdElement = document.createElement(cellElement.elementType);
+        tdElement.innerText=cellElement.value;
+        trElement.append(tdElement);
+    })
+    console.log(trElement);
+    tableBody.append(trElement);
+    })
+};
+dataStoreAdd();
+
+const addThead = () => {
+  const addTh = document.createElement("th");
+  const columnCount = head.childElementCount;
+  addTh.innerText = `column ${columnCount + 1}`;
+  head.append(addTh);
+  if (tableBody.childElementCount) {
+    for (const elem of tableBody.children) {
+      const td = document.createElement("td");
+      // td.innerText =`Row`
+      elem.appendChild(td);
+      td.append(createInput());
     }
-}
+  }
+};
 
-const GET_URL =(idInputVal) => `https://jsonplaceholder.typicode.com/todos/${idInputVal}`
-
-const addLiElement = (liText) =>{
-    const liTagElement = document.createElement("li");
-   liTagElement.innerText =liText;
-   todoList.append(liTagElement);
-}
-
-const getApiData =async(URL)=>{
-    const response = await fetch(URL)
-   const data = await response.json();
-   return data;
-}
-
-const handleAddTaskSubmit = async(event) =>{
-    event.preventDefault();
-   const idInputVal = idInput.value;
-    if(idInputVal ==='' || idInputVal <= 0 || idInputVal > 200){
-    alert('Enter a vaild id')
-     submitBtn.innerText ="Add"
-    return;
+const addTbody = () => {
+  const rowCount = tableBody.childElementCount + 1;
+  tableBody.innerHTML = "";
+  for (let j = 0; j < rowCount; j++) {
+    const addTr = document.createElement("tr");
+    for (let i = 1; i <= head.childElementCount; i++) {
+      const addTd = document.createElement("td");
+      // addTd.innerText =`Row ${i}`
+      addTr.append(addTd);
+      addTd.append(createInput());
     }
-   submitBtn.innerText="Adding"
-   const todoData = await getApiData(GET_URL(idInputVal))
-   if(!todoData.title){
-    alert('failed to get data!')
-   }
-   addLiElement(todoData.title);
-   todoLiData.push(todoData.title)
-   
-   idInput.value ='';
-   submitBtn.innerText ="Add"
-   console.log(todoList.innerHTML);
-}
-console.log(todoLiData);
-const handleSearchInput =(event) =>{
-    const searchVal = event.target.value;
-    searchArr(searchVal)
-}
+    tableBody.append(addTr);
+  }
+};
+const createInput = () => {
+  const inputTag = document.createElement("input");
 
-const searchArr = (searchVal) =>{
-    clearTodoList()
-    todoLiData.filter((element) => {
-       if(element.includes(searchVal)) {
-        addLiElement(element)
-       }
-    });
-}
+  return inputTag;
+};
